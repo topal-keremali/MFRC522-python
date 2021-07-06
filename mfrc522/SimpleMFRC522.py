dae8f893cc506844bd9ced725fe8e5115dfc77d6
topal-keremali <50803741+topal-keremali@users.noreply.github.com>
2021-07-06 09:01:53 +0200

    Update SimpleMFRC522.py

--- mfrc522/SimpleMFRC522.py
+++ mfrc522/SimpleMFRC522.py
@@ -43,7 +43,7 @@ class SimpleMFRC522:
         return None, None
     id = self.uid_to_num(uid)
     self.READER.MFRC522_SelectTag(uid)
-    status = self.READER.MFRC522_Auth(self.READER.PICC_AUTHENT1A, 11, self.KEY, uid)
+    #status = self.READER.MFRC522_Auth(self.READER.PICC_AUTHENT1A, 11, self.KEY, uid)
     data = []
     text_read = ''
     if status == self.READER.MI_OK:
@@ -71,7 +71,7 @@ class SimpleMFRC522:
           return None, None
       id = self.uid_to_num(uid)
       self.READER.MFRC522_SelectTag(uid)
-      status = self.READER.MFRC522_Auth(self.READER.PICC_AUTHENT1A, 11, self.KEY, uid)
+      #status = self.READER.MFRC522_Auth(self.READER.PICC_AUTHENT1A, 11, self.KEY, uid)
       self.READER.MFRC522_Read(11)
       if status == self.READER.MI_OK:
           data = bytearray()
