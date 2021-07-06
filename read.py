7a61c16755da7161086895442dd8cc7d5e6016f6
Kerem Ali Topal <k.topal@cartelsol.com>
2021-07-06 09:35:13 +0200

    16byte to 4byte converter

--- read.py
+++ read.py
@@ -6,7 +6,9 @@ import RPi.GPIO as GPIO
 try:
     while True:
         print("Hold a tag near the reader")
+        len = len(reader.BLOCK_ADDRS)
         id, text = reader.read()
+        cut_text = text[0:(len*4)]
         print("ID: %s\nText: %s" % (id,text))
         sleep(5)
 except KeyboardInterrupt:
