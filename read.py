6210f90b0434609fa61d871aeba935802f4b52c3
Kerem Ali Topal <k.topal@cartelsol.com>
2021-07-06 09:38:18 +0200

    updated loop

--- read.py
+++ read.py
@@ -4,13 +4,11 @@ reader = SimpleMFRC522()
 import RPi.GPIO as GPIO
 
 try:
-    while True:
-        print("Hold a tag near the reader")
-        len = len(reader.BLOCK_ADDRS)
-        id, text = reader.read()
-        cut_text = text[0:(len*4)]
-        print("ID: %s\nText: %s" % (id,cut_text))
-        sleep(5)
+    print("Hold a tag near the reader")
+    len = len(reader.BLOCK_ADDRS)
+    id, text = reader.read()
+    cut_text = text[0:(len*4)]
+    print("ID: %s\nText: %s" % (id,cut_text))
 except KeyboardInterrupt:
     GPIO.cleanup()
     raise
